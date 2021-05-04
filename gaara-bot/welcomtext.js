aad7a6bef1785b5a39f817ba235b8a0a42a9ac3f
GaaraBot <cashx804@gmail.com>
2021-05-04 09:13:05 -0300

    First commit

---
+++ gaara-bot/welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `Iaee @${number}. Seja Bem vindo meu mano ${groupname}`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `Tchauu @${number}. isso me deixa triste 👋`
+}
+exports.bye = bye
